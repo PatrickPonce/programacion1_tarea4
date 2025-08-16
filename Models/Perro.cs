d1aa61708bcf5cddfbac628ec203413cb87c43f3
Patrick Ponce <patrick_ponce1@usmp.pe>
2025-08-15 19:38:08 -0500

    Mejoras

--- Models/Perro.cs
+++ Models/Perro.cs
@@ -5,6 +5,7 @@ namespace TuProyecto.Models
         public string Nombre { get; set; }
         public string Peso { get; set; }
         public string Tamano { get; set; }
+        public string Edad { get; set; }
         public string Imagen { get; set; }
     }
 }
